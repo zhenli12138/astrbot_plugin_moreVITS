d41f0e46a92c98ae2a4048cec9844e1958c7a6a9
达莉娅 <115987311+zhenli12138@users.noreply.github.com>
2025-02-12 19:23:43 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import re
 '''---------------------------------------------------'''
 @register("astrbot_plugin_moreVITS", "达莉娅",
           "硅基流动利用用户的参考音频进行文本转语音的功能，内置了一个测试用的三月七（填写api就可用）",
-          "1.0.6")
+          "v1.1.0")
 class MyPlugin(Star):
     def __init__(self, context: Context, config: dict):
         super().__init__(context)
@@ -23,30 +23,25 @@ class MyPlugin(Star):
         self.counter = 0
         self.flag_mode = 1
         self.enabled = True
+        self.trap = True
         self.config = config
         self.base64_audio = ''
         self.api_url = 'https://api.siliconflow.cn/v1'
         self.api_name = config.get('name', 'FunAudioLLM/CosyVoice2-0.5B')
         self.music_mp3 = config.get('参考音频文件', './data/plugins/astrbot_plugin_morevits/三月七.mp3')
-        self.music_text = config.get('参考音频文本',
-                                     '所有没见过的东西都要拍下来，这样就不忘啦，等等我！'
-                                     '这个列车长绝对喜欢。'
-                                     '唔，这个姿势，嗐，真是不会拍照呢。'
-                                     '茄子！怎么样怎么样，很有意思吧？'
-                                     '嗯？你怎么还在发呆？')
+        self.music_text = config.get('参考音频文本','')
         self.music_url = config.get('参考音频url', '')
+        self.music_text_file = config.get('参考音频文本txt模式', '')
         self.api_key = config.get('apikey', '')
-
+        if os.path.exists(self.music_text_file):
+            with open(self.music_text_file, 'r',encoding="utf-8") as file:
+                self.music_text = file.read()
         if not self.api_name:
             self.api_name = 'FunAudioLLM/CosyVoice2-0.5B'
         if not self.music_mp3:
                 self.music_mp3 = './data/plugins/astrbot_plugin_morevits/三月七.mp3'
         if not self.music_text:
-            self.music_text = ('所有没见过的东西都要拍下来，这样就不忘啦，等等我！'
-                               '这个列车长绝对喜欢。'
-                               '唔，这个姿势，嗐，真是不会拍照呢。'
-                               '茄子！怎么样怎么样，很有意思吧？'
-                               '嗯？你怎么还在发呆？')
+            self.music_text = "所有没见过的东西都要拍下来，这样就不会忘了，等等我。这个列车长绝对喜欢。怎么样怎么样，很有意思吧？你怎么还在发呆？嘿嘿，第一张合照就归我啦。你没事吧，听得清我说话吗？记不记得自己叫什么名字？那可麻烦了，能努力回忆一下吗，你的名字是？那你自己小心哦。要不你把这个拿上吧。"
         if not self.music_url:
             self.music_url = ''
 
@@ -86,6 +81,27 @@ class MyPlugin(Star):
         self.music_text = music_text
         yield event.plain_result(f"参考音频文本上传完成")
 
+    @filter.command("过滤开关")
+    async def trap(self, event: AstrMessageEvent):
+        '''这是一个过滤颜表情开关指令'''
+        message_chain = event.get_messages()  # 用户所发的消息的消息链
+        logger.info(message_chain)
+        user_id = event.get_sender_id()
+        chain1 = [
+            At(qq=user_id),  # At 消息发送者
+            Plain(f"\n过滤已经启动"),
+            Face(id=337),
+        ]
+        chain2 = [
+            At(qq=user_id),  # At 消息发送者
+            Plain(f"\n过滤已经关闭"),
+            Face(id=337),
+        ]
+        self.trap = not self.trap
+        if self.trap:
+            yield event.chain_result(chain1)
+        else:
+            yield event.chain_result(chain2)
     '''---------------------------------------------------'''
 
     @filter.command("vitspro")
@@ -131,6 +147,10 @@ class MyPlugin(Star):
             logger.info(f"非LLM消息,pass")
             return
         logger.info(f"LLM返回的文本是：{text}")
+        result.chain.remove(Plain(text))
+        if self.trap:
+            text = self.remove_complex_emoticons(text)
+            logger.info(f"过滤颜表情后的文本是：{text}")
         adapter_name = event.get_platform_name()
         if not self.enabled:
             return
@@ -161,7 +181,6 @@ class MyPlugin(Star):
             output_audio_path = self.output.get()
             self.trash.put(output_audio_path)
             logger.info(f"转语音任务成功执行1次，队列中还有【{self.output.qsize()}】条语音待执行")
-            result.chain.remove(Plain(text))
             voice = MessageChain()
             voice.chain.append(Record(file=output_audio_path))
             await event.send(voice)
@@ -171,23 +190,16 @@ class MyPlugin(Star):
             await event.send(chain3)
             return
 
-    def remove_emoticons(text):
-        # 定义正则表达式，匹配常见的颜表情
-        emoticon_pattern = r"""
-            [<>]?                # 可选的 < 或 >
-            [:;=8xX]             # 眼睛部分
-            [\-o\*\']?           # 可选的鼻子部分
-            [\)\]\(\[dDpP/\:\}\{@\|\\]  # 嘴巴部分
-            |                   # 或
-            [\)\]\(\[dDpP/\:\}\{@\|\\]  # 嘴巴部分
-            [\-o\*\']?           # 可选的鼻子部分
-            [:;=8xX]             # 眼睛部分
-            [<>]?                # 可选的 < 或 >
+    def remove_complex_emoticons(self,text):
+        pattern = r"""
+                [a-zA-Z]                # 匹配所有英文字母
+                |                       # 或
+                \([^()]+\)              # 匹配括号内的复杂颜表情
+                |                       # 或
+                [^\u4e00-\u9fff，。？！、]  # 匹配非中文、非标点符号、非空格的字符
         """
-        # 使用 re.VERBOSE 允许正则表达式中的注释和换行
-        pattern = re.compile(emoticon_pattern, re.VERBOSE)
-        # 使用 sub 方法将匹配到的颜表情替换为空字符串
-        cleaned_text = pattern.sub('', text)
+        regex = re.compile(pattern, re.VERBOSE)
+        cleaned_text = regex.sub('', text)
         return cleaned_text
 
     def dynamic_timbre(self,text):
